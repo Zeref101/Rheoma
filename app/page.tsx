d69e6f4f03a2260d1c7f23e6b48925851b6af4b3
Zeref101 <shreyas.mohanty03@gmail.com>
2025-11-22 15:44:25 +0530

    feat: Set up Prisma with PostgreSQL, added initial database schema and migration files

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
-import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query";
-import { useTRPC } from "./trpc/routers/client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getQueryClient, trpc } from "./trpc/routers/server";
 import Client from "./client";
 import { Suspense } from "react";
+// import prisma from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
   const queryClient = getQueryClient();
 
   void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 
