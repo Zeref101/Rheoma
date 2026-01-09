566202abbafb3a366220abffc521866385982f68
Zeref101 <shreyas.mohanty03@gmail.com>
2026-01-09 13:27:53 +0530

    tsc fix

--- hooks/use-entity-search.tsx
+++ hooks/use-entity-search.tsx
@@ -43,6 +43,7 @@ export function useEntitySearch<
   }, [localSearch, params.search, debounceMs, params, setParams]);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setLocalSearch(params.search);
   }, [params.search]);
 
